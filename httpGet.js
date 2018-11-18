175cfc4ceaeb0aafb8e60dbf26ebf83d819e0ff4
Willaim <1340440725fhs@gmail.com>
2018-11-18 22:02:27 +0800

    minor: add console output support

--- httpGet.js
+++ httpGet.js
@@ -1,16 +1,20 @@
 'use strict';
-
+// require('node');
 const https = require('https');
 
 module.exports = async function getData(url, writeStream) {
   https
     .get(url, async (res) => {
       if (!res.statusCode === 200) return;
+      let body = '';
       try {
         for await (const chunk of res) {
-          process.stdout.write(chunk);
+          // process.stdout.write(chunk);
           writeStream.write(chunk);
+          body += chunk;
         }
+        console.dir(JSON.parse(body));
+        body = null;
       } catch (error) {
         console.error('Response stream error', error);
       }
