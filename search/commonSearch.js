905ac825de1095f4701192670cb5c9616ae535e1
Willaim <1340440725fhs@gmail.com>
2018-11-19 20:44:10 +0800

    minor: update

--- search/commonSearch.js
+++ search/commonSearch.js
@@ -13,7 +13,6 @@ module.exports = async function search(type, query) {
     type === match[3]
       ? commonUrl + `/${type}/${encodeURIComponent(query)}`
       : commonUrl + `/${type}/${encodeURIComponent(query)}/${page}`;
-  console.log(url);
   const dataPath = path.resolve(__dirname, `../data/${type}.json`);
   const writeStream = fs.createWriteStream(dataPath);
   await getData(url, writeStream);
